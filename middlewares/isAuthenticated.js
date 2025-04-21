7737db34efbd7f6001df191a4134dc493d751e3b
Harsh <ishaankatariya0581@gmail.com>
2025-04-21 18:40:47 +0530

    auth

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -2,26 +2,26 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        console.log("token", req.cookies);
-        
         const token = req.cookies.token;
+
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
                 success: false,
-            })
+            });
         }
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
-        if(!decode){
-            return res.status(401).json({
-                message:"Invalid token",
-                success:false
-            })
-        };
-        req.id = decode.userId;
+
+        const { userId } = jwt.verify(token, process.env.SECRET_KEY);
+        req.id = userId;
+
         next();
     } catch (error) {
-        console.log(error);
+        console.error("Auth Error:", error);
+        return res.status(401).json({
+            message: "Authentication failed. Invalid or expired token.",
+            success: false,
+        });
     }
-}
+};
+
 export default isAuthenticated;
